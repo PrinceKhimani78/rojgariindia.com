e65c6a5088af8382241a048b7c2c967bca73838d
PrinceKhimani186 <princekhimani186@gmail.com>
2025-12-03 12:50:50 +0530

    Add new deployment system + start.sh and server.js

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Load .env.local
+cd /home/rojgariindia.com/app
+
+# Load environment variables
 if [ -f .env.local ]; then
   export $(grep -v '^#' .env.local | xargs)
 fi
 
+echo ">>> Starting Next.js on port 3010"
 exec /usr/bin/npm start -- -p 3010 -H 0.0.0.0
