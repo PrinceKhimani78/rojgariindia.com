3b6b224f3d6271f9953a959d21b18e43db21f301
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-11 18:36:12 +0530

    ssl

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-APP_DIR="/home/rojgariindia.com/app/release"
+APP_DIR="/home/rojgariindia.com/app"
 
 cd $APP_DIR
 
-# Load environment variables if exist
+# Load env variables if exist
 if [ -f .env.local ]; then
   export $(grep -v '^#' .env.local | xargs)
 fi
 
-echo ">>> Starting Next.js on port 3010"
+echo ">>> Starting rojgariindia.com Next.js on port 3010"
 exec npm start -- -p 3010 -H 0.0.0.0
