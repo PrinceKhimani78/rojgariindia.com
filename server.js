2c8e0deb82eb2cf957a88a0c8bc95ed7c64d71d0
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-15 13:13:16 +0530

    gitpush

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const { createServer } = require("http");
 const next = require("next");
 
-const port = 3010;
 const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3020;
 
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
   }).listen(port, "0.0.0.0", (err) => {
-    if (err) throw err;
-    console.log(`>>> Next.js app running on port ${port}`);
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`> Main site ready on http://0.0.0.0:${port}`);
   });
 });
