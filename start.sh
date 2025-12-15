4ce367e5fc4649264efc24ff1fcf03a5406208c5
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-15 16:44:37 +0530

    aaa

--- start.sh
+++ start.sh
@@ -5,9 +5,8 @@ export NODE_ENV=production
 export PORT=3020
 
 APP_DIR="/home/rojgariindia.com/app"
+cd "$APP_DIR"
 
-cd $APP_DIR
-
-echo ">>> Starting rojgariindia.com Next.js custom server on port $PORT"
+echo ">>> Starting rojgariindia.com Next.js on port $PORT"
 
 exec node server.js
